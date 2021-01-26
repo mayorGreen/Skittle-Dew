7c72879c2bfc5e92d9685a1378ca9768dcd2ab97
mayorGreen <46832469+mayorGreen@users.noreply.github.com>
2021-01-26 08:18:33 -0800

    Fixed issue where Getter.php would give 500 server error
    Made changes to the Getter.php file so that the connection to the database did not close immediately after being called causing a 500 server error.

--- Front End/public_html/getter.php
+++ Front End/public_html/getter.php
@@ -1,31 +1,33 @@
 
 <?php
 
-$serverName = "WIN-RUH4VKA5M7L\SQLEXPRESS"; //serverName\instanceName
-$connectionInfo = array( "Database"=>"Buchanan County P.O. System", "UID"=>"sa", "PWD"=>"@MissouriWestern");
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+    echo "hello world \n";
+    $serverName = "WIN-RUH4VKA5M7L\SQLEXPRESS"; //serverName\instanceName
+    $connectionInfo = array("Database" => "Buchanan County P.O. System", "UID" => "sa", "PWD" => "@MissouriWestern");
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-if( !$conn )
-{
-    die(print_r(sqlsrv_errors(), true));
-}
-
-$sql = "SELECT Vendor_Name FROM Vendor_Table";
-
-$query = sqlsrv_query($conn, $sql);
-
-    if (!$query) {
+    if (!$conn) {
         die(print_r(sqlsrv_errors(), true));
     }
-    $nameArray = array();
 
-    while ($data = sqlsrv_fetch_array($query, SQLSRV_FETCH_ASSOC)) {
-        $name = $data['Vendor_Name'];
-        $nameArray[] = array("name" => $name);
+    $sql = "SELECT Vendor_Name FROM Vendor_Table";
+
+    $stmt = sqlsrv_query($conn, $sql);
+
+    if (!$stmt) {
+        die(print_r(sqlsrv_errors(), true));
     }
 
-    sqlsvr_close($conn);
-    echo json_encode($nameArray);
+    $nameArray = array();
+
+    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
+    {
+        echo json_encode($row['Vendor_Name']);
+    }
+
+    sqlsrv_free_stmt($stmt);
+    echo "finished";
+
 
 
 
