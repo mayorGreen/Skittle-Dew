0240689bdf2247063312da5e9a5f8ccc7f0d1793
mayorGreen <46832469+mayorGreen@users.noreply.github.com>
2021-01-26 09:39:15 -0800

    Getter now gets vendor_id and vendor_name
    Added functionality so that getter gets vendor info 1 of 2 ways. 
    Single echo(the uncommented echo) - "vendor_id vendor_name"
    2 echos(the commented out ones) - "vendor_id" "vendor_name"
    comment or uncomment to choose which way you would like to recieve and parse the data

---
+++ getter.php
@@ -0,0 +1,36 @@
+
+<?php
+
+    echo "hello world \n";
+    $serverName = "WIN-RUH4VKA5M7L\SQLEXPRESS"; //serverName\instanceName
+    $connectionInfo = array("Database" => "Buchanan County P.O. System", "UID" => "sa", "PWD" => "@MissouriWestern");
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+    if (!$conn) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    $sql = "SELECT Vendor_ID, Vendor_Name FROM Vendor_Table";
+
+    $stmt = sqlsrv_query($conn, $sql);
+
+    if (!$stmt) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    $nameArray = array();
+
+    while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
+    {
+        //echo json_encode($row['Vendor_ID']);
+        //echo json_encode($row['Vendor_Name']);
+        echo json_encode($row['Vendor_ID'].' '.$row['Vendor_Name']);
+
+    }
+
+    sqlsrv_free_stmt($stmt);
+    echo "finished";
+
+
+
+
