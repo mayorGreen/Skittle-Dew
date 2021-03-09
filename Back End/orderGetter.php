adbd6c0b63c3adf7200b15148df35926aa594d20
Jacob <jtjonnybravo@gmail.com>
2021-03-08 18:14:18 -0600

    Changed orderGetter.php for better table setup, formatting, and so that all line items from a vendor's order during specified times are shown.

--- Back End/orderGetter.php
+++ Back End/orderGetter.php
@@ -8,17 +8,18 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 if (!$conn) {
     die(print_r(sqlsrv_errors(), true));
 }
+//Variables from user input
 $contract = $_COOKIE["conSelect"];
 $vendorID = $_COOKIE["vID"];
 $vendorName = $_COOKIE["vName"];
-$orderDate1 = $_COOKIE["date1"];
-$orderDate2 = $_COOKIE["date2"];
+//1 day subracted from "from date" and added to "to date" so that "Between" in query works properly
+$orderDate1 = date("Y-m-d", strtotime("-1 day", strtotime($_COOKIE["date1"])));
+$orderDate2 = date("Y-m-d", strtotime("+1 day", strtotime($_COOKIE["date2"])));
 $vendorTotal = 0.0;
 
 //SQL for all orders
 $sql = "SELECT Order_ID FROM Order_Table WHERE Vendor_ID = ? AND Order_Date Between ? AND ? ";
 $params = array($vendorID,$orderDate1,$orderDate2);
-
 
 $stmt = sqlsrv_query($conn, $sql, $params);
 
@@ -32,45 +33,85 @@ if (!$stmt) {
  * If we do not need these they can be taken out or left in in case we decide to use them later.
  *
  */
+//SQL for contract orders
+$sql1 = "Select * FROM Line_Item_Table WHERE Order_ID in (?";
+
+//SQL for non-contract orders
+$sql3 = "Select * FROM Line_Item_Table WHERE Order_ID in (?";
+
+//SQL for all orders
+$sql2 = "Select * FROM Line_Item_Table WHERE Order_ID in (?";
+
+
+$a = ",?";
+$params2 = array();
+$numRows = 0;
+
+//Appends order ids to params2 and adds an extra variable to each sql statement
+//very sloppy usage, but it works
 while($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC))
 {
     $id2 = $row['Order_ID'];
+    array_push($params2, $id2);
+    $numRows ++;
 }
-//SQL for contract orders
-$sql1 = "Select * FROM Line_Item_Table WHERE Order_ID = ? AND Contract_Number != 0";
-
-//SQL for non-contract orders
-$sql3 = "Select * FROM Line_Item_Table WHERE Order_ID = ? AND Contract_Number = 0";
-
-//SQL for all orders
-$sql2 = "Select * FROM Line_Item_Table WHERE Order_ID = ?";
-$params2 = array($id2);
-
-if($contract == "Contract")
+for($i = 1; $i <$numRows; $i++)
 {
+    //SQL for contract orders
+    $sql1 .= $a;
+
+    //SQL for non-contract orders
+    $sql3 .= $a;
+
+    //SQL for all orders
+    $sql2 .= $a;
+}
+
+//Returns reports based on whether an item has a contract or not
+if(strcmp($contract, "Contract") == 0)
+{
+    $sql1 .=") AND Contract_Number != 0";
     $stmt2 = sqlsrv_query($conn, $sql1, $params2);
-}elseif($contract == "Non-Contract")
+}elseif(strcmp($contract,"Non-Contract") == 0)
 {
+    $sql3 .= ") AND Contract_Number = 0";
     $stmt2 = sqlsrv_query($conn, $sql3, $params2);
-}else $stmt2 = sqlsrv_query($conn, $sql2, $params2);
+}else
+{
+    $sql2 .= ")";
+    $stmt2 = sqlsrv_query($conn, $sql2, $params2);
+}
 
 if (!$stmt2) {
     die(print_r(sqlsrv_errors(), true));
 }
 
-$html1 = "<html> <head> <style>
-table {font-family: arial, sans-serif; border-collapse: collapse; width: 100%;}
-td, th { border: 1px solid #dddddd; text-align: center; padding: 4px;}
-tr:nth-child() {background-color: #dddddd;}
-</style>
-</head>
+//reverts dates back to user selected dates
+$orderDate1 = date('F d, Y', strtotime("+1 day", strtotime($orderDate1)));
+$orderDate2 = date('F d, Y', strtotime("-1 day", strtotime($orderDate2)));
+
+
+$html1 = "<html> 
+    <head> 
+        <link rel='icon' href='../Front%20End/public_html/favicon.ico'>
+        <link rel='stylesheet' href='../Front%20End/public_html/style.css'>
+        <title>PO System</title>
+        <style>
+            table {font-family: arial, sans-serif; border-collapse: collapse;width: 75%;}
+            td, th { border: 1px solid #dddddd; text-align: center; padding: 4px;}
+            tr:nth-child() {background-color: #dddddd;}
+        </style>
+    </head>
 <body>
 <div id='info'>
-<h2>Report for $vendorName</h2>
-<h3>Between $orderDate1 and $orderDate2</h3>
+    <h5 align='center'>
+        <img src='../Front%20End/public_html/favicon.ico'>
+    </h5>
+    <h2 align='center'>$vendorName Purchase Report</h2>
+    <h3 align='center'>For orders between: $orderDate1 and $orderDate2 </h3>
 </div>
 <div id='tab'>
-<table>
+<table align='center'>
   <tr>
     <th>Line Item</th>
     <th>Contract</th>
@@ -97,6 +138,11 @@ while($row = sqlsrv_fetch_array($stmt2, SQLSRV_FETCH_ASSOC))
     $itemPrice = $row['Item_Price'];
     $itemTotal = $row['Total_Price'];
 
+    if($contract == 0)
+    {
+        $contract = "Non-Contract";
+    }
+
     $vendorTotal += $itemTotal;
 
     echo "<tr><td>$line_item</td>";
@@ -108,16 +154,23 @@ while($row = sqlsrv_fetch_array($stmt2, SQLSRV_FETCH_ASSOC))
     echo "<td>$itemTotal</td></tr>";
 }
 
-echo "</table> 
+echo " <tfoot>
+        <tr>
+            <th colspan='5'></th>
+            <th id = 'total' colspan='1'>Total </th>
+            <td>$vendorTotal</td>
+        </tr>
+      </tfoot>
+      </table> 
       </div> 
       <div id='endInfo'>
-      <h3>Total spent by $vendorName between $orderDate1 and $orderDate2 is: $$vendorTotal</h3>
+      <br>
       </div>
-      <form action='../Front%20End/public_html/tabs.html'>
-      <input type='submit' value='Return Home'>
-      </form>
-      <p>
-      <input type='button' value='Create PDF' id='btPrint' onclick='createPDF()'/>
+        <form align='center' action='../Front%20End/public_html/tabs.html'>
+            <input type='submit' value='Return Home'>
+        </form>
+      <p align= 'center'>
+        <input type='button' value='Create PDF' id='btPrint' onclick='createPDF()'/>
       </p>
       </body>
       <script>
